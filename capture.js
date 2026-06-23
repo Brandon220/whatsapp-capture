0be43d47ea5a3a77990c226a52df5e8ac1d1addd
Brandon220 <brandongarcia20052313@gmail.com>
2026-06-23 03:47:18 -0600

    feat: captura con MongoDB y OpenWA

---
+++ capture.js
@@ -0,0 +1,14 @@
+{
+  "name": "whatsapp-capture",
+  "version": "1.0.0",
+  "type": "module",
+  "description": "Captura de datos con MongoDB y OpenWA",
+  "main": "capture.js",
+  "scripts": {
+    "start": "node capture.js"
+  },
+  "dependencies": {
+    "mongodb": "^6.0.0",
+    "node-fetch": "^3.3.2"
+  }
+}
